955ceb5b4b2b4184ef7de41242c9b7310a13557e
VISHAL RAJ <87539634+VR903@users.noreply.github.com>
2023-11-23 00:04:26 +0530

    1

---
+++ curdpro/src/main/java/com/example22/curdpro/CurdproApplication.java
@@ -0,0 +1,13 @@
+package com.example22.curdpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurdproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurdproApplication.class, args);
+	}
+
+}
---
+++ curdpro/src/test/java/com/example22/curdpro/CurdproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example22.curdpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurdproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
